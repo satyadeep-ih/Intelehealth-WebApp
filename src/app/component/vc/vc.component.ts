f6741f69fd8f86beec048df95eb76388cdc5929d
Zeeshan <zeeshan@intelehealth.org>
2021-03-24 23:37:28 +0530

    webrtc client connect to apprtc

--- src/app/component/vc/vc.component.ts
+++ src/app/component/vc/vc.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
 import { SocketService } from "src/app/services/socket.service";
-import Peer from "simple-peer";
-import { environment } from "../../../environments/environment";
-declare var AppController: any;
-
 @Component({
   selector: "app-vc",
   templateUrl: "./vc.component.html",
@@ -14,73 +9,29 @@ export class VcComponent implements OnInit {
   @ViewChild("remoteVideo") remoteVideoRef: any;
   @ViewChild("localVideo") localVideoRef: any;
 
-  private userid: string = "";
-  private localStream: any;
-  private playing: boolean = false;
-  private streamRecieved: boolean = false;
   callerStream: any;
-  myStream;
+  localStream;
   myId;
   callerSignal;
   callerInfo;
   incomingCall;
+  isInitiator;
+  isStarted;
+  pc;
+  isChannelReady;
+  room = "foo";
 
-  constructor(
-    private route: ActivatedRoute,
-    public socketService: SocketService
-  ) {}
+  constructor(public socketService: SocketService) {}
 
   ngOnInit(): void {
     this.socketService.initSocket();
     this.startUserMedia();
-    this.socketService.onEvent("myId").subscribe((id) => {
-      this.myId = id;
-      console.log(" this.myId: ", this.myId);
-    });
-
-    this.socketService.onEvent("join").subscribe((config) => {
-      console.log("config: ", config);
-      // const loadingParams = {
-      //   errorMessages: config.error_messages ? config.error_messages : [],
-      //   warningMessages: config.warning_messages ? config.warning_messages : [],
-      //   isLoopback: config.is_loopback,
-      //   roomId: config.room_id,
-      //   roomLink: config.room_link,
-      //   mediaConstraints: config.media_constraints,
-      //   offerOptions: config.offer_options,
-      //   peerConnectionConfig: config.pc_config,
-      //   peerConnectionConstraints: config.pc_constraints,
-      //   iceServerRequestUrl: config.turn_url,
-      //   iceServerTransports: config.turn_transports,
-      //   wssUrl: config.wss_url,
-      //   wssPostUrl: config.wss_post_url,
-      //   bypassJoinConfirmation: config.bypass_join_confirmation,
-      //   versionInfo: config.version_info ? config.version_info : [],
-      //   roomServer: environment.socketURL,
-      // };
-      // console.log("environment.socketURL: ", environment.socketURL);
-      // this.appRtc = new AppController(loadingParams);
-      // console.log("this.appRtc: ", this.appRtc);
-    });
-
-    // this.socketService.onEvent("hey").subscribe((data) => {
-    //   console.log("incomingCall:hey ", data);
-    //   this.incomingCall = true;
-    //   this.callerInfo = data.from;
-    //   this.callerSignal = data.signal;
-    // });
-
-    // this.socketService.turn().subscribe((res) => {
-    //   const iceServers = JSON.parse(res).iceServers;
-    //   console.log("iceServers: ", iceServers);
-    //   this.iceServers = iceServers;
-    // });
+    this.socketService.onEvent("myId").subscribe((id) => (this.myId = id));
+    this.initSocketEvents();
+    this.socketService.emitEvent("create or join", this.room);
+    console.log("Attempted to create or  join room", this.room);
   }
-  appRtc;
 
-  get users() {
-    return this.socketService.activeUsers;
-  }
   isStreamAvailable;
   startUserMedia(config?: any, cb = () => {}): void {
     let mediaConfig = {
@@ -88,7 +39,7 @@ export class VcComponent implements OnInit {
         width: { min: 1024, ideal: 1280, max: 1920 },
         height: { min: 576, ideal: 720, max: 1080 },
       },
-      audio: false,
+      audio: true,
     };
 
     if (config) {
@@ -104,8 +55,8 @@ export class VcComponent implements OnInit {
     n.getUserMedia(
       mediaConfig,
       (stream: MediaStream) => {
-        this.myStream = stream;
-        this.localVideoRef.nativeElement.srcObject = this.myStream;
+        this.localStream = stream;
+        this.localVideoRef.nativeElement.srcObject = this.localStream;
         cb();
       },
       (err) => {
@@ -115,173 +66,186 @@ export class VcComponent implements OnInit {
     );
   }
 
-  roomId = "12121215";
-  params = null;
-  join() {
-    this.startUserMedia({ video: true }, () => {
-      console.log("->>>started");
-      this.socketService.join(this.roomId).subscribe((res) => {
-        if (res.result === "SUCCESS") {
-          this.params = res.params;
-          console.log("this.params: ", this.params);
-          this.appRtcCreatePeer();
-        } else {
-          console.log("connection already exists: ", this.params);
+  initSocketEvents() {
+    this.socketService.onEvent("message").subscribe((room) => {
+      this.isInitiator = true;
+    });
+
+    this.socketService.onEvent("message").subscribe((room) => {
+      console.log("Room " + room + " is full");
+    });
+
+    this.socketService.onEvent("join").subscribe((room) => {
+      console.log("Another peer made a request to join room " + room);
+      console.log("This peer is the initiator of room " + room + "!");
+      this.isChannelReady = true;
+    });
+
+    this.socketService.onEvent("joined").subscribe((room) => {
+      console.log("joined: " + room);
+      this.isChannelReady = true;
+    });
+
+    /**
+     * log
+     */
+    this.socketService.onEvent("log").subscribe((array) => {
+      console.log("array:log ", array);
+      console.log.apply(console, array);
+    });
+
+    /**
+     * message
+     */
+    this.socketService.onEvent("message").subscribe((message) => {
+      console.log("Client received message:", message);
+      if (message === "got user media") {
+        this.maybeStart();
+      } else if (message.type === "offer") {
+        if (!this.isInitiator && !this.isStarted) {
+          this.maybeStart();
         }
-      });
+        this.pc.setRemoteDescription(new RTCSessionDescription(message));
+        this.doAnswer();
+      } else if (message.type === "answer" && this.isStarted) {
+        this.pc.setRemoteDescription(new RTCSessionDescription(message));
+      } else if (message.type === "candidate" && this.isStarted) {
+        var candidate = new RTCIceCandidate({
+          sdpMLineIndex: message.label,
+          candidate: message.candidate,
+        });
+        this.pc.addIceCandidate(candidate);
+      } else if (message === "bye" && this.isStarted) {
+        this.handleRemoteHangup();
+      }
     });
   }
-  iceServers: [
-    // {
-  //   urls: ["turn:40.80.93.209:3478"];
-  //   username: "chat";
-  //   credential: "nochat";
-  // },
-  // {
-  //   urls: ["turn:numb.viagenie.ca"];
-  //   username: "sultan1640@gmail.com";
-  //   credential: "98376683";
-  // },
-  // { urls: ["stun:stun.l.google.com:19302"] },
-  // { urls: ["stun:stun1.l.google.com:19302"] }
-  ];
-  peer = null;
-  appRtcCreatePeer() {
-    this.startUserMedia();
 
-    this.peer = new Peer({
-      initiator: true,
-      trickle: false,
-      config: {
-        iceServers: this.iceServers,
-        stream: this.myStream,
-      },
-    });
-
-    this.peer.on("signal", (data) => {
-      console.log("signal: from remote ", data);
-      const message = JSON.stringify(data);
-      this.peer.signal(message);
-      this.socketService
-        .message(this.roomId, this.params.client_id, message)
-        .subscribe((res) => {
-          console.log("res:message ", res);
-        });
-    });
-
-    this.peer.on("stream", (stream) => {
-      console.log("stream:from -> remotestream ", stream);
-      this.remoteVideoRef.nativeElement.srcObject = stream;
-      this.callerStream = stream;
-    });
-
-    if (this.params.messages.length > 0) {
-      this.params.messages.forEach((msg) => {
-        const signal = JSON.parse(msg);
-        console.log("signal: to remote", signal);
-        this.peer.signal(signal);
-      });
+  maybeStart() {
+    console.log(
+      ">>>>>>> maybeStart() ",
+      this.isStarted,
+      this.localStream,
+      this.isChannelReady
+    );
+    if (
+      !this.isStarted &&
+      typeof this.localStream !== "undefined" &&
+      this.isChannelReady
+    ) {
+      console.log(">>>>>> creating peer connection");
+      this.createPeerConnection();
+      this.pc.addStream(this.localStream);
+      this.isStarted = true;
+      console.log("isInitiator", this.isInitiator);
+      if (this.isInitiator) {
+        this.doCall();
+      }
     }
   }
 
-  activeCallerId = null;
-  candidate = false;
-  call(userId): void {
-    this.activeCallerId = userId;
-    console.log("this.activeCallerId: ", this.activeCallerId);
-    const peer = new Peer({
-      initiator: true,
-      trickle: false,
-      config: {
+  createPeerConnection() {
+    try {
+      this.pc = new RTCPeerConnection({
         iceServers: [
-          // {
-          //   urls: "stun:numb.viagenie.ca",
-          //   username: "sultan1640@gmail.com",
-          //   credential: "98376683",
-          // },
           {
-            urls: "turn:numb.viagenie.ca",
+            urls: ["turn:40.80.93.209:3478"],
+            credential: "nochat",
+            username: "chat",
+          },
+          {
+            urls: ["turn:numb.viagenie.ca"],
             username: "sultan1640@gmail.com",
             credential: "98376683",
           },
-          { urls: "stun:stun.l.google.com:19302" },
-          { urls: "stun:stun1.l.google.com:19302" },
+          { urls: ["stun:stun.l.google.com:19302"] },
+          { urls: ["stun:stun1.l.google.com:19302"] },
         ],
-      },
-      stream: this.myStream,
-    });
-
-    this.socketService.onEvent("message").subscribe((data) => {
-      if (data.from === "app") {
-        console.log("message:-->>>> ", data);
-        if (data.type === "offer") {
-          const offer = {
-            type: data.type,
-            sdp: data.sdp,
-          };
-          peer.signal(offer);
-          this.candidate = false;
-        }
-        if (!this.candidate && data.type === "candidate") {
-          this.candidate = true;
-          console.log("message:candidate-->>>> ", data);
-          peer.signal(data);
-        }
-      }
-    });
-
-    peer.on("signal", (data) => {
-      console.log("signal: call from us ", data);
-      this.socketService.emitEvent("message", {
-        signalData: data,
-        from: this.myId,
-        userToCall: userId,
-        to: this.activeCallerId,
       });
-    });
-
-    peer.on("connect", (data) => {
-      console.log("connect>>>>data: ", data);
-    });
-    peer.on("data", (data) => {
-      console.log("data:data>>>>> ", data);
-    });
-
-    peer.on("stream", (stream: MediaStream) => {
-      console.log("stream:from -> remotestream ", stream);
-      // const remoteStream = new MediaStream();
-      // remoteStream.addTrack(stream.getVideoTracks()[0]);
-      this.callerStream = stream;
-    });
-
-    // this.socketService.onEvent("callAccepted").subscribe((signal) => {
-    //   console.log("signal: ", signal);
-    //   peer.signal(signal);
-    // });
+      this.pc.onicecandidate = this.handleIceCandidate.bind(this);
+      this.pc.onaddstream = this.handleRemoteStreamAdded.bind(this);
+      this.pc.onremovestream = this.handleRemoteStreamRemoved.bind(this);
+      console.log("Created RTCPeerConnnection");
+    } catch (e) {
+      console.log("Failed to create PeerConnection, exception: " + e.message);
+      alert("Cannot create RTCPeerConnection object.");
+      return;
+    }
   }
 
-  acceptCall(): void {
-    const peer = new Peer({
-      initiator: false,
-      trickle: false,
-      stream: this.myStream,
-    });
-    this.incomingCall = false;
+  handleRemoteStreamRemoved(event) {
+    console.log("Remote stream removed. Event: ", event);
+  }
 
-    peer.on("signal", (data) => {
-      console.log("data: remote", data);
-      this.socketService.emitEvent("acceptCall", {
-        signal: data,
-        to: this.callerInfo,
+  handleRemoteStreamAdded(event) {
+    console.log("Remote stream added.");
+    const remoteStream = event.stream;
+    this.remoteVideoRef.nativeElement.srcObject = remoteStream;
+  }
+
+  handleIceCandidate(event) {
+    console.log("icecandidate event: ", event);
+    if (event.candidate) {
+      this.sendMessage({
+        type: "candidate",
+        label: event.candidate.sdpMLineIndex,
+        id: event.candidate.sdpMid,
+        candidate: event.candidate.candidate,
       });
-    });
+    } else {
+      console.log("End of candidates.");
+    }
+  }
 
-    peer.on("stream", (stream) => {
-      console.log("stream:remote ", stream);
-      this.remoteVideoRef.nativeElement.srcObject = stream;
-      this.callerStream = stream;
-    });
+  doCall() {
+    console.log("Sending offer to peer");
+    this.pc.createOffer(
+      this.setLocalAndSendMessage.bind(this),
+      this.handleCreateOfferError.bind(this)
+    );
+  }
 
-    peer.signal(this.callerSignal);
+  handleCreateOfferError(event) {
+    console.log("createOffer() error: ", event);
+  }
+
+  doAnswer() {
+    console.log("Sending answer to peer.");
+    this.pc
+      .createAnswer()
+      .then(
+        this.setLocalAndSendMessage.bind(this),
+        this.onCreateSessionDescriptionError.bind(this)
+      );
+  }
+
+  setLocalAndSendMessage(sessionDescription) {
+    this.pc.setLocalDescription(sessionDescription);
+    console.log("setLocalAndSendMessage sending message", sessionDescription);
+    this.sendMessage(sessionDescription);
+  }
+
+  onCreateSessionDescriptionError(error) {
+    console.log("Failed to create session description: " + error.toString());
+  }
+
+  sendMessage(data) {
+    this.socketService.emitEvent("message", data);
+  }
+
+  handleRemoteHangup() {
+    console.log("Session terminated.");
+    stop();
+    this.isInitiator = false;
+  }
+
+  stop() {
+    this.isStarted = false;
+    this.pc.close();
+    this.pc = null;
+  }
+
+  get users() {
+    return this.socketService.activeUsers;
   }
 }
